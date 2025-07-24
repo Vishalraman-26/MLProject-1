fdf2556b2a49b9ca07332ab8af82db3e38c0313f
Vishalraman-26 <ramanvishal66@gmail.com>
2025-07-25 00:41:12 +0530

    model triner

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,7 +18,6 @@ from src.exception import CustomException
 from src.logger import logging
 
 from src.utils import save_object,evaluate_models
-
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path=os.path.join("artifacts","model.pkl")
